35c4cc539b7d927ae5526f59267d71cbc4cdd7f2
bei.liu <1725287754@qq.com>
2017-07-21 15:19:47 +0800

    Python入门

---
+++ Python基础.py
@@ -0,0 +1,21 @@
+#输出
+#print(100+200)
+#print('The quick brown fox','jumps over','the lazy dog')
+#print('100 + 200 =',100+200)
+
+#每一行都是一个语句，当语句以冒号“：”结尾时，缩进的语句视为代码块，使用4个空格的缩进
+a=100
+if a>=0:
+    print(a)
+else:
+    print(-a)
+
+
+
+
+
+
+
+
+
+
