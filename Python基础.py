550eb8f6ee6e4b1a4a4d6660fd58ed841de63ad1
bei.liu <1725287754@qq.com>
2017-07-26 10:20:55 +0800

    Python入门

--- Python基础.py
+++ Python基础.py
@@ -4,11 +4,89 @@
 #print('100 + 200 =',100+200)
 
 #每一行都是一个语句，当语句以冒号“：”结尾时，缩进的语句视为代码块，使用4个空格的缩进
-a=100
-if a>=0:
-    print(a)
-else:
-    print(-a)
+#a=100
+#if a>=0:
+    #print(a)
+#else:
+    #print(-a)
+
+
+
+#字符串
+#
+#print('I\'m ok!')
+
+#print('I\'m leaning\npython')
+
+#print('\\\n\\')
+
+# r''表示''内部的字符串默认不转义
+#print(r'\\\n\\')
+#print('\\\t\\')
+
+#print('''line1
+#line2
+#line3''')
+
+#布尔值
+#age=20
+#if age>18:
+    #print('adult')
+#else:
+   # print('teenager')
+
+#“=”赋值，同一个变量可以反复赋值，而且可以是不同类型的变量
+#a=123
+#print(a)
+#a='ABC'
+#print(a)
+#a=456
+#print(a)
+
+n=123
+print(n)
+s1='Hello,world'
+print(s1)
+s2='Hello,\'Admin\''
+print(s2)
+s3=r'Hello,"Bart"'
+print(s3)
+s4=r'''Hello,
+Lisa!'''
+print(s4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
