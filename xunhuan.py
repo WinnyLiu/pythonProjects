ab63602808123aadccc27b609dca5b935d218f24
bei.liu <1725287754@qq.com>
2017-08-02 16:51:51 +0800

    Python入门

---
+++ xunhuan.py
@@ -0,0 +1,65 @@
+# names=['Winny','Ada','Bob']
+# for name in names:
+#     print(name)
+
+
+# sum=0
+# L=[1,2,3,4,5,6,7,8,9,10]
+# for a in L:
+#     sum=sum+a
+#     print(sum)
+
+
+
+# sum=0
+# #range(101):生成0-100的整数序列
+# for x in range(101):
+#     sum=sum+x
+#     print(sum)
+
+
+
+# sum=0
+# n=99
+# while n>0:
+#     sum=sum+n
+#     n=n-2
+# print(sum)
+
+
+#练习
+# L=['Winny','Ada','Bob']
+# for x in L:
+#     print('Hello,'+x)
+
+
+# n=1
+# while n<=100:
+#     if n>10:
+#         break
+#     print(n)
+#     n=n+1
+# print('END')
+
+
+n=0
+while n<10:
+    n=n+1
+    if n%2==0:
+        continue
+    print(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
